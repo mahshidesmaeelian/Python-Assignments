082d9e73a37a0f21c7788c2b4ac019b8c6582f4c
Mahshid Esmaeelian <88210093+mahshidesmaeelian@users.noreply.github.com>
2021-10-20 15:26:08 +0330

    Add files via upload

--- Assignment 19/symmetrical_list.py
+++ Assignment 19/symmetrical_list.py
@@ -1,11 +1,16 @@
 n=int(input("Enter number:"))
+
 temp=n
 rev=0
+
 while(n>0):
+    
     dig = n%10
     rev = rev*10+dig
     n = n//10
+
 if(temp == rev):
-    print("The number is a palindrome!")
+    print("The numbers are symmetric!")
+
 else:
     print("The numbers are not symmetric!")
